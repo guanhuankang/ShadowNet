f0ca3e78021c665cfbb736a08427a7a55c9bcfa2
guanhuankang <2659814334@qq.com>
2023-03-22 15:55:01 +0800

    add slurm.sh

---
+++ slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=special_cs
+#SBATCH --nodes=1                # 1 computer nodes
+#SBATCH --ntasks-per-node=1      # 1 MPI tasks on EACH NODE
+#SBATCH --cpus-per-task=4        # 4 OpenMP threads on EACH MPI TASK
+#SBATCH --gres=gpu:1             # Using 1 GPU card
+#SBATCH --mem=64GB               # Request 50GB memory
+#SBATCH --time=0-23:59:00        # Time limit day-hrs:min:sec
+#SBATCH --output=output.log   # Standard output
+#SBATCH --error=error.err    # Standard error log
+
+pwd
+nvidia-smi
+python train.py
