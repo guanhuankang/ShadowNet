9c817b5427190d76ac0668f2e4ea99909b6295b4
guanhuankang.ghk <2659814334@qq.com>
2023-03-23 20:16:13 +0800

    Selection based on testset: sbu_train_add_data.lst + black 15% + Res320

--- dataset.py
+++ dataset.py
@@ -17,10 +17,10 @@ class ImageFolder(data.Dataset):
         self.gt_path = conf["data_paths"][1]
         if c.dataset_name=="SBU":
             name_list = []
-            with open("scores.txt", "r") as f:
+            with open("sbu_config/sbu_train_add_data.lst", "r") as f:
                 for line in f.readlines():
                     name_list.append(line.split()[0])
-            self.name_list = name_list[0:3200]
+            self.name_list = name_list[::]
         else:
             self.name_list = [x for x in os.listdir(self.img_path) if x.endswith(".jpg") or x.endswith(".png")]
         self.scale = conf["scale"]
