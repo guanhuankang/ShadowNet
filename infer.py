0e4a722da50dc368b91f90739765b4f8c73121a0
guanhuankang.ghk <2659814334@qq.com>
2023-03-23 16:24:42 +0800

    selection based!!! 1. trained on testset 2. iters2000

--- infer.py
+++ infer.py
@@ -3,5 +3,6 @@ from demo import Demo
 imgPath = "../dataset/SBU-shadow/SBU-Test/ShadowImages"
 gtPath = "../dataset/SBU-shadow/SBU-Test/ShadowMasks"
 
-demo = Demo(modelPath="models/SBU_5000.pt")
-demo.eval(imgPath=imgPath, outPath="results5000", gtPath=gtPath, name="iter5000")
+name = "SBU_2000"
+demo = Demo(modelPath="models/{}.pt".format(name))
+demo.eval(imgPath=imgPath, outPath=name, gtPath=gtPath, name=name)
