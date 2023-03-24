c8604ade8c5b2428424f28c95aea7215e5c33f1a
guanhuankang.ghk <2659814334@qq.com>
2023-03-24 16:42:34 +0800

    fix bug: convert->RGB/L extendTrain: SBU+man (5040+repeat3200) 1. Res320 2. black15%, patch=16x16

--- dataset.py
+++ dataset.py
@@ -43,9 +43,9 @@ class ImageFolder(data.Dataset):
             gt = torchvision.transforms.functional.hflip(gt)
 
         ## randomly corrupt images
-        n = 8
+        n = 16
         h, w = self.scale[0]//n, self.scale[1]//n
-        corruption = torch.rand(1, 1, h, w).lt(0.70).float()
+        corruption = torch.rand(1, 1, h, w).lt(0.85).float()
         corruption_mask = torch.nn.functional.interpolate(corruption, size=self.scale, mode="nearest")[0]
         img = img * corruption_mask
 
