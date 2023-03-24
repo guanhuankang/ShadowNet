2d63d6627e0a9142a1c56338b2dc091cc00c4868
guanhuankang.ghk <2659814334@qq.com>
2023-03-24 18:52:00 +0800

    remove randomcrop: extendTrain: SBU-extTrain (3200/2500/1500/1000) 1. Res320 2. black15%, patch=16x16

--- dataset.py
+++ dataset.py
@@ -21,7 +21,7 @@ class ImageFolder(data.Dataset):
             with open("sbu_config/scores.txt", "r") as f:
                 for line in f.readlines():
                     name_list.append(line.split()[0])
-            self.name_list = name_list[0:4085] + name_list[0:3200] + name_list[0:2500]
+            self.name_list = name_list[0:3200] + name_list[0:2500] + name_list[0:1500] + name_list[0:1000]
         else:
             self.name_list = [x for x in os.listdir(self.img_path) if x.endswith(".jpg") or x.endswith(".png")]
         print("dataset len", len(self.name_list))
